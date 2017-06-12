23ae41e2b8e9243a2579d70e5239b1f0080d2109
Jardel Silveira <jardel@ufc.br>
2017-06-12 16:20:53 -0300

    Create monta.bat

--- EXAMPLE_SoC/ARMSOC_1/Software/monta.bat
+++ EXAMPLE_SoC/ARMSOC_1/Software/monta.bat
@@ -2,4 +2,4 @@ type cabeca.v > ahb2mem_v2.v
 v2sv cm0dsasm.v miolo 4095
 type miolo.sv >> ahb2mem_v2.v
 type rabo.v >> ahb2mem_v2.v
-type ahb2mem_v2.v > ..\..\..\AHB_Peripherals\AHBL_MEM\ahb2mem_v2.v
+type ahb2mem_v2.v > ..\Xilinx\ARMSOC_1.srcs\sources_1\imports\CM0DS-DesignKit\AHB_Peripherals\AHBL_MEM\ahb2mem_v2.v
