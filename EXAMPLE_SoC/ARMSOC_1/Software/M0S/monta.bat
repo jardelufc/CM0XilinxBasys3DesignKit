2e1fac7d0c4ba5b1f6f48609c81dc028b6c58397
Jardel Silveira <jardel@ufc.br>
2017-06-12 16:20:06 -0300

    Update monta.bat

--- EXAMPLE_SoC/ARMSOC_1/Software/M0S/monta.bat
+++ EXAMPLE_SoC/ARMSOC_1/Software/M0S/monta.bat
@@ -3,4 +3,4 @@ echo @08000000 >> m0s.v
 ..\v2sv m0s.v miolo 4095
 type miolo.sv >> ahb2mem_v2.v
 type ..\rabo.v >> ahb2mem_v2.v
-type ahb2mem_v2.v > ..\..\..\..\AHB_Peripherals\AHBL_MEM\ahb2mem_v2.v
+type ahb2mem_v2.v > ..\..\Xilinx\ARMSOC_1.srcs\sources_1\imports\CM0DS-DesignKit\AHB_Peripherals\AHBL_MEM\ahb2mem_v2.v
